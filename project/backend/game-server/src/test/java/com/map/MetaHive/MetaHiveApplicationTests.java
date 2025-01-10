d800ed6a99e923ff95082a1a8463e5d91ac60def
SaidurIUT <saidurrahman@iut-dhaka.edu>
2025-01-10 23:23:44 +0600

    backend updated

---
+++ project/backend/game-server/src/test/java/com/map/MetaHive/MetaHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.map.MetaHive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
